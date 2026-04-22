72d15e1458a24a73ff557c33036f3cd321d2308f
llyu-s <eliasjsmith@outlook.com>
2026-04-21 23:33:39 -0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,10 @@
 import "./globals.css";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "llyus",
+  description: "Personal website for llyus",
+};
 
 export default function RootLayout({
   children,
